4d00a33bd8226a193f8643aeba4e43b10d87c412
lungtsai_lin <lungtsai_lin@htc.com>
2014-09-29 23:06:40 +0800

    net: wireless: bcmdhd: Send DISASSOC to FW if status of wlan is connecting
    b/17665436
    
    Signed-off-by: lungtsai_lin <lungtsai_lin@htc.com>
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/net/wireless/bcmdhd/wl_cfg80211.c
+++ drivers/net/wireless/bcmdhd/wl_cfg80211.c
@@ -4146,6 +4146,7 @@ wl_cfg80211_disconnect(struct wiphy *wiphy, struct net_device *dev,
 		if (cfg->scan_request) {
 			wl_notify_escan_complete(cfg, dev, true, true);
 		}
+		wl_clr_drv_status(cfg, CONNECTING, dev);
 		wl_set_drv_status(cfg, DISCONNECTING, dev);
 		if (wl_get_drv_status(cfg, CONNECTING, dev)) {
 			/* in case of associating status, this will abort assoc procedure */
